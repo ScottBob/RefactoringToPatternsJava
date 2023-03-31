f4dc63ed75bd28bd1a1ab1c060e8148b61d7ae6a
Scott Wierschem <scottw@potentialpower.com>
2023-03-31 05:35:51 -0500

    4) Replace hard-to-read xml appends with TagNode function calls

--- ReplaceImplicitTreeWithComposite/src/MyWork/OrdersWriter.java
+++ ReplaceImplicitTreeWithComposite/src/MyWork/OrdersWriter.java
@@ -56,12 +56,18 @@ public class OrdersWriter {
     }
 
     private void writePriceTo(StringBuilder xml, Product product) {
+        TagNode priceNode = new TagNode("price");
+        priceNode.addAttribute("currency", currencyFor(product));
+        priceNode.addValue(product.getPrice());
+        xml.append(priceNode.toString());
+        /*
         xml.append("<price");
         xml.append(" currency='");
         xml.append(this.currencyFor(product));
         xml.append("'>");
         xml.append(product.getPrice());
         xml.append("</price>");
+         */
     }
 
     private String currencyFor(Product product) {
