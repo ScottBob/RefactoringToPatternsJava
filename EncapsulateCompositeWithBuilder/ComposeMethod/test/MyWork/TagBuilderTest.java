3bd90a9b528e4cf837894518679ab8ed735e2969
Scott Wierschem <scottw@potentialpower.com>
2023-03-31 06:04:52 -0500

    3) Create failing test to handle children

--- EncapsulateCompositeWithBuilder/ComposeMethod/test/MyWork/TagBuilderTest.java
+++ EncapsulateCompositeWithBuilder/ComposeMethod/test/MyWork/TagBuilderTest.java
@@ -10,4 +10,15 @@ public class TagBuilderTest {
         String actual = new TagBuilder("flavors").toXml();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void test_build_one_child() {
+        String expected = "<flavors>" +
+                             "<flavor/>" +
+                          "</flavors>";
+        TagBuilder builder = new TagBuilder("flavors");
+        builder.addChild("flavor");
+        String actual = builder.toXml();
+        Assert.assertEquals(expected, actual);
+    }
 }
