664a17caa6d7b299778091edf615783197120319
Scott Wierschem <scottw@potentialpower.com>
2023-03-31 06:27:20 -0500

    6) To handle siblings, we need to understand parents first. Write a failing test to support parent queries.

--- EncapsulateCompositeWithBuilder/ComposeMethod/test/MyWork/TagBuilderTest.java
+++ EncapsulateCompositeWithBuilder/ComposeMethod/test/MyWork/TagBuilderTest.java
@@ -40,15 +40,26 @@ public class TagBuilderTest {
     }
 
     @Test
-    public void test_build_sibling() {
-        String expected = "<flavors>" +
-                             "<flavor1/>" +
-                             "<flavor2/>" +
-                          "</flavors>";
-        TagBuilder builder = new TagBuilder("flavors");
-        builder.addChild("flavor1");
-        builder.addSibling("flavor2");
-        String actual = builder.toXml();
-        Assert.assertEquals(expected, actual);
+    public void test_parents() {
+        TagNode root = new TagNode("root");
+        Assert.assertNull(root.getParent());
+
+        TagNode childNode = new TagNode("child");
+        root.add(childNode);
+        Assert.assertEquals(root, childNode.getParent());
+        Assert.assertEquals("root", childNode.getParent().getName());
     }
+
+//    @Test
+//    public void test_build_sibling() {
+//        String expected = "<flavors>" +
+//                             "<flavor1/>" +
+//                             "<flavor2/>" +
+//                          "</flavors>";
+//        TagBuilder builder = new TagBuilder("flavors");
+//        builder.addChild("flavor1");
+//        builder.addSibling("flavor2");
+//        String actual = builder.toXml();
+//        Assert.assertEquals(expected, actual);
+//    }
 }
