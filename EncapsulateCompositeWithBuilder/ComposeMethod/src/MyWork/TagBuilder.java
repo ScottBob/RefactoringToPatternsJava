e623041564304f20993b7e8ba379bed98dfeb3b5
Scott Wierschem <scottw@potentialpower.com>
2023-03-31 06:07:28 -0500

    4) Make test pass by handling chidren in TagBuilder

--- EncapsulateCompositeWithBuilder/ComposeMethod/src/MyWork/TagBuilder.java
+++ EncapsulateCompositeWithBuilder/ComposeMethod/src/MyWork/TagBuilder.java
@@ -2,12 +2,20 @@ package MyWork;
 
 public class TagBuilder {
     private TagNode rootNode;
+    private TagNode currentNode;
 
     public TagBuilder(String rootTagName) {
         rootNode = new TagNode(rootTagName);
+        currentNode = rootNode;
     }
 
     public String toXml() {
         return rootNode.toString();
+    }
+
+    public void addChild(String childTagName) {
+        TagNode parentNode = currentNode;
+        currentNode = new TagNode(childTagName);
+        parentNode.add(currentNode);
     }
 }
