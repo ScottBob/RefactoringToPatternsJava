38e7424a6633968a59ef6a071f51040faf45ad18
Scott Wierschem <scottw@potentialpower.com>
2023-03-31 03:15:53 -0500

    2) Move capital() calculation code from Loan to CapitalStrategy

--- ReplaceConditionalLogicWithStrategy/src/MyWork/CapitalStrategy.java
+++ ReplaceConditionalLogicWithStrategy/src/MyWork/CapitalStrategy.java
@@ -2,6 +2,17 @@ package MyWork;
 
 public class CapitalStrategy {
     public double capital() {
+        if (
+            return commitment * duration() * riskFactor();
+        }
+        if (expiry != null && maturity == null) {
+            if (getUnusedPercentage() != 1.0) {
+                return commitment * getUnusedPercentage() * duration() * riskFactor();
+            } else {
+                return (outstandingRiskAmount() * duration() * riskFactor())
+                        + (unusedRiskAmount() * duration() * unusedRiskFactor());
+            }
+        }
         return 0.0;
     }
 }
