3038b3e309c7c202ee50743d08cec093dbec8e66
Scott Wierschem <scottw@potentialpower.com>
2023-07-14 13:01:13 -0500

    8) Create an abstract class, Handler, for the others to inherit from.

---
+++ ReplaceConditionalDispatcherWithCommand/src/MyWork/Handler.java
@@ -0,0 +1,13 @@
+package MyWork;
+
+import java.util.Map;
+
+public abstract class Handler {
+    protected CatalogApp catalogApp;
+
+    public Handler(CatalogApp catalogApp) {
+        this.catalogApp = catalogApp;
+    }
+
+    public abstract HandlerResponse execute(Map parameters);
+}
